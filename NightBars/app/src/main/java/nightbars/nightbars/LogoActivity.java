ef978858e9bafddbbd62591c141e8c32b000f08f
Martina <martinella17@gmail.com>
2017-05-17 00:31:48 +0200

    colors & things..

--- NightBars/app/src/main/java/nightbars/nightbars/LogoActivity.java
+++ NightBars/app/src/main/java/nightbars/nightbars/LogoActivity.java
@@ -1,7 +1,28 @@
 package nightbars.nightbars;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class LogoActivity extends FragmentActivity {
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LogoActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+
 
 }
--- NightBars/app/src/main/java/nightbars/nightbars/PlacesListActivity.java
+++ NightBars/app/src/main/java/nightbars/nightbars/PlacesListActivity.java
@@ -122,19 +122,7 @@ public class PlacesListActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        } else if (id == R.id.logout) {
+        if (id == R.id.logout) {
             sessionManager.setLogin(false);
 
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
